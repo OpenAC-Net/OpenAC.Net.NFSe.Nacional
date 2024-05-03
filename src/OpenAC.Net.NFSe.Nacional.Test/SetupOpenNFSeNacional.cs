ed0b5f88fb8a97dd469135f02bc5c2318d70a282
Lucas Moraes <lucasmoraes804@gmail.com>
2024-05-03 18:06:44 -0300

    feat: Adicionar arquivo de setup

---
+++ src/OpenAC.Net.NFSe.Nacional.Test/SetupOpenNFSeNacional.cs
@@ -0,0 +1,13 @@
+﻿namespace OpenAC.Net.NFSe.Nacional.Test;
+
+public class SetupOpenNFSeNacional
+{
+    public static void SetSetup(OpenNFSeNacional openNFSeNacional)
+    {
+        openNFSeNacional.Configuracoes.Certificados.SelecionarCertificado();
+        openNFSeNacional.Configuracoes.Geral.Salvar = true;
+        openNFSeNacional.Configuracoes.Geral.RetirarAcentos = true;
+        openNFSeNacional.Configuracoes.Geral.RetirarEspacos = true;
+        openNFSeNacional.Configuracoes.Arquivos.PathSalvar = 
+    }
+}
