2b5320b8c5ea58cdcd8fafb72c5653ba458c8905
Marcos Gerene Felix Spirito <mgf.spirito@gmail.com>
2026-01-05 10:47:29 -0300

    ..

--- src/OpenAC.Net.NFSe.Nacional.Console/Program.cs
+++ src/OpenAC.Net.NFSe.Nacional.Console/Program.cs
@@ -1,42 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-using OpenAC.Net.DFe.Core.Document;
-using OpenAC.Net.NFSe.Nacional.Webservice;
-
-Console.WriteLine("Hello, World!");
+﻿using OpenAC.Net.NFSe.Nacional.Webservice;
 
 NFSeServiceManager manager = NFSeServiceManager.Instance;
 
-NFSeServiceInfo infoNacional = new NFSeServiceInfo()
-{
-    UF  = OpenAC.Net.DFe.Core.Common.DFeSiglaUF.EX,
-    Nome = "NFSe Nacional",
-    Codigo = -1,
-    Provider = NFSeProvider.Nacional,
-};
-
-NFSeEnvironment homoNacional = new();
-homoNacional.Ambiente = OpenAC.Net.DFe.Core.Common.DFeTipoAmbiente.Homologacao;
-homoNacional.Enderecos.Add(OpenAC.Net.NFSe.Nacional.Common.Types.TipoUrl.Enviar, "https://sefin.producaorestrita.nfse.gov.br/SefinNacional");
-homoNacional.Enderecos.Add(OpenAC.Net.NFSe.Nacional.Common.Types.TipoUrl.EnviarEvento, "https://sefin.producaorestrita.nfse.gov.br/SefinNacional");
-homoNacional.Enderecos.Add(OpenAC.Net.NFSe.Nacional.Common.Types.TipoUrl.ConsultarNsu, "https://adn.producaorestrita.nfse.gov.br/contribuintes");
-homoNacional.Enderecos.Add(OpenAC.Net.NFSe.Nacional.Common.Types.TipoUrl.ConsultarChave, "https://adn.producaorestrita.nfse.gov.br/contribuintes");
-homoNacional.Enderecos.Add(OpenAC.Net.NFSe.Nacional.Common.Types.TipoUrl.DownloadDanfse, "https://sefin.producaorestrita.nfse.gov.br/SefinNacional");
-homoNacional.Enderecos.Add(OpenAC.Net.NFSe.Nacional.Common.Types.TipoUrl.ConsultarChaveDps, "https://sefin.producaorestrita.nfse.gov.br/SefinNacional");
-homoNacional.Enderecos.Add(OpenAC.Net.NFSe.Nacional.Common.Types.TipoUrl.ConsultaExisteDps, "https://sefin.producaorestrita.nfse.gov.br/SefinNacional");
-infoNacional.Ambientes.Add(homoNacional);
-
-NFSeEnvironment prodNacional = new();
-prodNacional.Ambiente = OpenAC.Net.DFe.Core.Common.DFeTipoAmbiente.Producao;
-prodNacional.Enderecos.Add(OpenAC.Net.NFSe.Nacional.Common.Types.TipoUrl.Enviar, "https://sefin.nfse.gov.br/sefinnacional");
-prodNacional.Enderecos.Add(OpenAC.Net.NFSe.Nacional.Common.Types.TipoUrl.EnviarEvento, "https://sefin.nfse.gov.br/sefinnacional");
-prodNacional.Enderecos.Add(OpenAC.Net.NFSe.Nacional.Common.Types.TipoUrl.ConsultarNsu, "https://adn.nfse.gov.br/contribuintes");
-prodNacional.Enderecos.Add(OpenAC.Net.NFSe.Nacional.Common.Types.TipoUrl.ConsultarChave, "https://adn.nfse.gov.br/contribuintes");
-prodNacional.Enderecos.Add(OpenAC.Net.NFSe.Nacional.Common.Types.TipoUrl.DownloadDanfse, "https://sefin.nfse.gov.br/sefinnacional");
-prodNacional.Enderecos.Add(OpenAC.Net.NFSe.Nacional.Common.Types.TipoUrl.ConsultarChaveDps, "https://sefin.nfse.gov.br/sefinnacional");
-prodNacional.Enderecos.Add(OpenAC.Net.NFSe.Nacional.Common.Types.TipoUrl.ConsultaExisteDps, "https://sefin.nfse.gov.br/sefinnacional");
-infoNacional.Ambientes.Add(prodNacional);
-
-manager.Services.Webservices.Add(infoNacional);
+/*
+Exemplo de como adicionar uma cidade:
 
 NFSeServiceInfo infoDoisCorregos = new NFSeServiceInfo()
 {
@@ -69,7 +36,6 @@ prodDoisCorregos.Enderecos.Add(OpenAC.Net.NFSe.Nacional.Common.Types.TipoUrl.Con
 infoDoisCorregos.Ambientes.Add(prodDoisCorregos);
 
 manager.Services.Webservices.Add(infoDoisCorregos);
-
-string xml = manager.Services.GetXml();
+*/
 
 manager.Save(@"c:\temp\Municipios.nfse");
